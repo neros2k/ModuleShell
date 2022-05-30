9cca130d1300bf1b53c8a965ed1755cd95b057f1
neros2k <neros2k@gmail.com>
2022-05-30 15:37:19 +0300

    Обратно вернул.

--- src/main/java/n2k_/moduleshell/base/AbstractListener.java
+++ src/main/java/n2k_/moduleshell/base/AbstractListener.java
@@ -6,20 +6,25 @@ import java.util.Arrays;
 public abstract class AbstractListener extends ListenerAdapter implements Initilizable {
     private final AbstractModule MODULE;
     private final JDA JDA;
+
     public AbstractListener(AbstractModule module, JDA jda) {
         this.MODULE = module;
         this.JDA = jda;
     }
+
     @Override
     public void init() {
         JDA.addEventListener(this);
     }
+
     protected boolean isValid(String id) {
         return Arrays.stream(ModuleShell.getEnabledModulesMap().get(id)).toList().contains(this.MODULE.getID());
     }
+
     public AbstractModule getModule() {
         return this.MODULE;
     }
+
     public JDA getJDA() {
         return this.JDA;
     }
