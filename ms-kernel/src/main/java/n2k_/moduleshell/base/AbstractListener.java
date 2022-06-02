5d3333d26a57a1d26e1e7a6be3daf1ea900beb6f
neros2k <neros2k@gmail.com>
2022-06-03 02:42:15 +0300

    Реализация базового класса слушателя событий.

--- ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractListener.java
+++ ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractListener.java
@@ -1,3 +1,18 @@
 package n2k_.moduleshell.base;
-public abstract class AbstractListener {
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+public abstract class AbstractListener extends ListenerAdapter implements Initilizable {
+    private final AbstractModule MODULE;
+
+    public AbstractListener(AbstractModule module) {
+        this.MODULE = module;
+    }
+
+    @Override
+    public void init() {
+        this.MODULE.addListener(this);
+    }
+
+    public AbstractModule getModule() {
+        return this.MODULE;
+    }
 }
