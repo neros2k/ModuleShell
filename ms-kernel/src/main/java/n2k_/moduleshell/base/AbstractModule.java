dd86c31616519ea4d1979b6b2d02d81c49eb94b4
neros2k <neros2k@gmail.com>
2022-06-03 02:42:01 +0300

    Реализация базового класса простейшего модуля.

--- ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractModule.java
+++ ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractModule.java
@@ -1,4 +1,53 @@
 package n2k_.moduleshell.base;
-public abstract class AbstractModule {
+import net.dv8tion.jda.api.JDA;
+import java.util.ArrayList;
+import java.util.List;
+public abstract class AbstractModule implements Initilizable {
+    private final JDA JDA;
+    private final String NAME;
+    private final String DESCRIPTION;
+    private final List<AbstractListener> LISTENER_LIST;
+    private final List<AbstractCommand> COMMAND_LIST;
 
+    public AbstractModule(JDA jda, String name, String description) {
+        this.JDA = jda;
+        this.NAME = name;
+        this.DESCRIPTION = description;
+        this.LISTENER_LIST = new ArrayList<>();
+        this.COMMAND_LIST = new ArrayList<>();
+    }
+
+    @Override
+    public void init() {
+        this.LISTENER_LIST.addAll(this.COMMAND_LIST);
+        this.LISTENER_LIST.forEach(AbstractListener::init);
+    }
+
+    void addListener(AbstractListener listener) {
+        this.LISTENER_LIST.add(listener);
+    }
+
+    void addCommand(AbstractCommand command) {
+        this.COMMAND_LIST.add(command);
+    }
+
+    public JDA getJDA() {
+        return this.JDA;
+    }
+
+    public String getName() {
+        return this.NAME;
+    }
+
+    public String getDescription() {
+        return this.DESCRIPTION;
+    }
+
+    public List<AbstractListener> getListenerList() {
+        return this.LISTENER_LIST;
+    }
+
+    public List<AbstractCommand> getCommandList() {
+        return this.COMMAND_LIST;
+    }
 }
