d8b3269c544786b54095a701749c27ab648c474f
neros2k <neros2k@gmail.com>
2022-06-03 02:42:28 +0300

    Реализация базового класса простейший команд.

--- ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractCommand.java
+++ ms-kernel/src/main/java/n2k_/moduleshell/base/AbstractCommand.java
@@ -1,3 +1,37 @@
 package n2k_.moduleshell.base;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
 public abstract class AbstractCommand extends AbstractListener {
+    private final String NAME;
+    private final String DESCRIPTION;
+    private final String PREFIX;
+
+    public AbstractCommand(AbstractModule module, String name, String description, String prefix) {
+        super(module);
+        this.NAME = name;
+        this.DESCRIPTION = description;
+        this.PREFIX = prefix;
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentDisplay().split(" ");
+        if(args[0].equals(this.PREFIX+this.NAME)) {
+            this.onCommandCall(new CommandContext(args, event, this));
+        }
+    }
+
+    public abstract void onCommandCall(CommandContext ctx);
+
+    public String getName() {
+        return this.NAME;
+    }
+
+    public String getDescription() {
+        return this.DESCRIPTION;
+    }
+
+    public String getPrefix() {
+        return this.PREFIX;
+    }
 }
